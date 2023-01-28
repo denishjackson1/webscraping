2a835c83836c0826f5b634513adf61fd8b8992ab
denishjackson1 <denishkomakech@gmail.com>
2023-01-28 12:32:25 +0300

    code

---
+++ website.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "http://olympus.realpython.org/profiles/aphrodite"
+
+page = requests.get(URL)
+html = page.text
+
+# Create a BeautifulSoup Object
+soup = BeautifulSoup(html, 'html.parser')
+
+#Extract text from title
+title = soup.find_all('title')
+for t in title:
+    print(t.get_text())
+
+# Extract text from body.
+body = soup.find_all('center')
+for c in body:
+    print(c.get_text())
